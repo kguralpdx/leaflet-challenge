ff1fe05872c41e5c231238a202feb350e667f63a
kguralpdx <66285376+kguralpdx@users.noreply.github.com>
2020-10-25 16:32:44 -0700

    added Images folder and then screenshot file of Level 1 map, finished Level 1 write-up in README

--- Leaflet-Step-1/static/js/logic.js
+++ Leaflet-Step-1/static/js/logic.js
@@ -91,7 +91,7 @@ function createFeatures(earthquakeData) {
 
 function createMap(earthquakes, mags) {
 
-  // Define streetmap and darkmap layers
+  // Define streetmap layer
   var lightmap = L.tileLayer("https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
     attribution: "© <a href='https://www.mapbox.com/about/maps/'>Mapbox</a> © <a href='http://www.openstreetmap.org/copyright'>OpenStreetMap</a> <strong><a href='https://www.mapbox.com/map-feedback/' target='_blank'>Improve this map</a></strong>",
     tileSize: 512,
@@ -100,13 +100,6 @@ function createMap(earthquakes, mags) {
     id: "mapbox/light-v10",
     accessToken: API_KEY
   });
-
-//   var darkmap = L.tileLayer("https://api.mapbox.com/styles/v1/mapbox/{id}/tiles/{z}/{x}/{y}?access_token={accessToken}", {
-//     attribution: "Map data &copy; <a href=\"https://www.openstreetmap.org/\">OpenStreetMap</a> contributors, <a href=\"https://creativecommons.org/licenses/by-sa/2.0/\">CC-BY-SA</a>, Imagery © <a href=\"https://www.mapbox.com/\">Mapbox</a>",
-//     maxZoom: 18,
-//     id: "dark-v10",
-//     accessToken: API_KEY
-//   });
 
   // Define a baseMaps object to hold our base layers
   var baseMaps = {
